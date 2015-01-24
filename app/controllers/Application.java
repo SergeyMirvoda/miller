f9f3ca0dd089e342b818dd3695983000a1fab82c
Vladimir Miller <vladimir.miller.ru@gmail.com>
2015-01-25 02:33:24 +0500

    Увеличено колличество собираемой информации

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,19 +1,20 @@
 package controllers;
 
+import com.avaje.ebean.Ebean;
 import com.fasterxml.jackson.databind.JsonNode;
-import models.ErrorReportModel;
+import models.*;
 import play.Logger;
 import play.data.Form;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Security;
-import views.html.index;
-import views.html.login;
-import views.html.viewerror;
+import views.html.*;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
-
 import static play.data.Form.form;
 
 public class Application extends Controller {
@@ -21,7 +22,8 @@ public class Application extends Controller {
     @Security.Authenticated(Secured.class)
     public static Result index() {
         try {
-            Logger.info("Отображение списка для apiKey: " +session().get("apiKey"));
+            Logger.info("list for ApiKey || Отображение списка для apiKey: " +session().get("apiKey"));
+            flash("success");
             return ok(index.render(ErrorReportModel.find
                                     .where()
                                     .eq("apiKey", session().get("apiKey"))
@@ -32,30 +34,77 @@ public class Application extends Controller {
             e.printStackTrace();
             throw e;
         }
+
     }
 
     @Security.Authenticated(Secured.class)
     public static Result errors(UUID id) {
         return ok(viewerror.render(ErrorReportModel.find.byId(id)));
     }
-    public static Result log() {
-        if (request().body() == null || request().body().asJson()==null){
-            System.out.println(request().body());
-            return badRequest("json value have to be provided:");
-        }
-        JsonNode json = request().body().asJson();
-        ErrorReportModel model = Json.fromJson(json, ErrorReportModel.class);
-        model.id = UUID.randomUUID();
-        ErrorReportModel.save(model);
-        return play.mvc.Results.ok();
-    }
 
+        public static Result log()  {
+            if (request().body() == null || request().body().asJson()==null){
+                System.out.println(request().body());
+                return badRequest("json value have to be provided:");
+            }
+            JsonNode json = request().body().asJson();
+            ErrorReportModel model = Json.fromJson(json, ErrorReportModel.class);
+            model.id = UUID.randomUUID();
+            model.happens = 1;
+            String x = model.getMessage();
+
+            List<ErrorReportModel> list = ErrorReportModel.find.where().eq("message", x).findList();// упорядочили по сообщению
+            int counter = list.size();
+
+            for (ErrorReportModel reportModel: list){
+                if (list.size() != 0){
+                    if (reportModel.happens == 1){
+                        int temp = counter;
+                        System.out.println("if message: " + reportModel.happens);
+                        model.setHappens(model.happens + 1);
+                        Ebean.delete(reportModel);
+                        Ebean.save(reportModel);
+                        Ebean.save(model);
+
+                    } else if (reportModel.happens > 1){
+                        int temp = counter;
+                        temp = temp -1;
+                        temp = temp + reportModel.happens;
+                        Ebean.delete(reportModel);
+                        Ebean.save(reportModel);
+                        model.setHappens(temp + 1);
+                        Ebean.save(model);
+                        System.out.println("else if message: " + reportModel.happens + " " + temp);
+                        System.out.println("temp in  " + temp);
+
+                    } else if (reportModel.happens == 0){
+                        Ebean.delete(reportModel);
+                        System.out.println("Something go wrong");
+                    }
+                } else if (list.size() == 0 ){
+                    Ebean.save(model);
+                }
+
+            }
+
+            flash("new report been added");
+            ErrorReportModel.save(model);
+            return play.mvc.Results.ok();
+        }
+
+
+    //Удалить отчет
+    public static Result remove(UUID id){
+       ErrorReportModel.find.ref(id).delete();
+       return redirect(routes.Application.index());
+    }
+    //Авторизация
     public static Result login() {
         return ok(
                 login.render(form(Login.class))
         );
     }
-
+    //Аутентификация
     public static Result authenticate() {
         Form<Login> loginForm = form(Login.class).bindFromRequest();
         if (loginForm.hasErrors()) {
@@ -69,15 +118,43 @@ public class Application extends Controller {
             );
         }
     }
-
-    public static Result register() {
-        return play.mvc.Results.TODO;
-    }
-
+    //Валидация введенных данных
     public static class Login {
-
         public String email;
         public String password;
 
+        public String validate(){
+            if (User.authenticate(email, password) == null){
+                return "Неверный адрес или пароль"; // i18
+            }
+            return null;
+        }
     }
+    //переадресация на регистрация
+    public static Result register(){
+        return ok(register.render(form(User.class)));
+    }
+    //Регистрация нового пользователя
+    public static Result newUser(){
+        Form<User> userForm = form(User.class).bindFromRequest();
+        if (userForm.hasErrors()){
+            return badRequest(register.render(userForm));
+        }else{
+            userForm.get().save();
+            return redirect(routes.Application.index());
+        }
+    }
+
+    //Выход из системы
+    public static Result logout(){
+        session().clear();
+        return redirect(routes.Application.login());
+    }
+
+
+//    О программе
+//    public static Result about() {
+//        return ok(about.render());
+//    }
+
 }
--- public/javascripts/errortracker.client.js
+++ public/javascripts/errortracker.client.js
@@ -2,6 +2,8 @@
  * Шаблон "Модуль"
  * http://www.adequatelygood.com/JavaScript-Module-Pattern-In-Depth.html
  * Created by mirvoda_sg on 03.12.2014.
+ *
+ *
  */
 var ET = (function () {
     var my = {},
@@ -16,16 +18,268 @@ var ET = (function () {
     };
     my.send = function(success){
         //todo: Здесь нужно научиться собирать StackTrace и другие параметры браузера
-        if (!apiKey){
-            throw new Error("Требуется вызвать метод init и передать ему ключ API")
+        var navAppVer = navigator.appVersion;
+        var navUsrAgt = navigator.userAgent;
+        var unknown = "unknown"; //Можно сделать проверку - если переменная "null" то забить ее Unknown
+
+        //Получение времени в удобном формате
+        function getTime(){
+        var d = new Date();
+        var hours = d.getHours();        if(hours<10){hours='0'+hours}
+        var minutes = d.getMinutes();    if(minutes<10){minutes='0'+minutes}
+        var seconds = d.getSeconds();    if(seconds<10){seconds='0'+seconds}
+        var day = d.getDate();        if(day<10){ day='0'+day}
+        var month = d.getMonth()+1;        if(month<10){month='0'+month}
+        var year = d.getFullYear();
+        var x = hours+':'+minutes+':'+seconds+' '+day+"/"+month+'/'+year
+        var toUtc = d.toISOString();
+        return {
+            Date: x,
+            UTC: toUtc
+        };
         }
 
+        function getScreenResolution(){
+
+            var screenResolution = window.screen.width + "X" + window.screen.height;
+            var
+                width = window.innerWidth ,
+                height = window.innerHeight,
+                colorD = window.screen.colorDepth;
+
+            return {
+
+                browserResolution: width + 'X' + height,
+                nativeScreenResolution: screenResolution,
+                colorDepth: colorD
+            };
+        }
+
+        //Получение информации о битности приложения
+        function getPlatformInfo(){
+            var verOfSet,
+                platform = window.navigator.platform;
+
+            if (verOfSet = navUsrAgt.indexOf("WOW64") != -1){
+                platform = "32-битное приложение работающее на 64-битном процессоре";
+
+            } else if (verOfSet = navUsrAgt.indexOf("Win64") != -1) {
+                platform = "64-битное приложение";
+
+            } else if (verOfSet = navUsrAgt.indexOf("amd64") != -1) {
+                platform = "64-битное приложение работающее под процессоре AMD";
+
+            } else if (verOfSet = navUsrAgt.indexOf("x86_64") != -1) {
+                platform = "64-битное приложение работающее на процессоре Intel"
+
+            } else if (verOfSet = navUsrAgt.indexOf("armv7") != -1) {
+                platform = "Операционная система работающая на процессоре ARM v7"
+            }
+
+            return {
+                platformString: platform
+            };
+        }
+
+
+        //получение характеристик браузера
+        function getBrowserInfo(){
+            var
+            browser = navigator.appName,
+            vendor = unknown,
+            version = '' + parseFloat(navAppVer),
+            majorVersion = parseInt(navAppVer, 10),
+            nameOffset, verOffset, ix;
+
+            // Opera
+            if ((verOffset = navUsrAgt.indexOf('Opera')) != -1) {
+                browser = 'Opera';
+                vendor = "OPERA SOFTWARE"
+                version = navUsrAgt.substring(verOffset + 6);
+                if ((verOffset = navUsrAgt.indexOf('Version')) != -1) {
+                    version = navUsrAgt.substring(verOffset + 8);
+                }
+            }
+            // Chrome
+            else if ((verOffset = navUsrAgt.indexOf('Chrome')) != -1) {
+                browser = 'Chrome';
+                version = navUsrAgt.substring(verOffset + 7);
+                vendor = "Google Inc"
+            }
+            // Safari
+            else if ((verOffset = navUsrAgt.indexOf('Safari')) != -1) {
+                browser = 'Safari';
+                vendor = "Apple Inc"
+                version = navUsrAgt.substring(verOffset + 7);
+                if ((verOffset = navUsrAgt.indexOf('Version')) != -1) {
+                    version = navUsrAgt.substring(verOffset + 8);
+                }
+            }
+            // Firefox
+            else if ((verOffset = navUsrAgt.indexOf('Firefox')) != -1) {
+                browser = 'Firefox';
+                vendor = "The Mozilla Foundation"
+                version = navUsrAgt.substring(verOffset + 8);
+            }
+            // IE ниже 11 версии
+            else if ((verOffset = navUsrAgt.indexOf('MSIE')) != -1) {
+                browser = 'Microsoft Internet Explorer';
+                version = navUsrAgt.substring(verOffset + 5);
+                vendor = "Microsoft Inc"
+            }
+            // MSIE 11+
+            else if (navUsrAgt.indexOf('Trident/') != -1) {
+                browser = 'Microsoft Internet Explorer';
+                vendor = "Microsoft Inc"
+                version = navUsrAgt.substring(navUsrAgt.indexOf('rv:') + 3);
+            }
+            // Другие браузеры
+            else if ((nameOffset = navUsrAgt.lastIndexOf(' ') + 1) < (verOffset = navUsrAgt.lastIndexOf('/'))) {
+                browser = navUsrAgt.substring(nameOffset, verOffset);
+                version = navUsrAgt.substring(verOffset + 1);
+                if (browser.toLowerCase() == browser.toUpperCase()) {
+                    browser = navigator.appName;
+                }
+            }
+            // trim the version string
+            if ((ix = version.indexOf(';')) != -1) version = version.substring(0, ix);
+            if ((ix = version.indexOf(' ')) != -1) version = version.substring(0, ix);
+            if ((ix = version.indexOf(')')) != -1) version = version.substring(0, ix);
+
+            majorVersion = parseInt('' + version, 10);
+            if (isNaN(majorVersion)) {
+                version = '' + parseFloat(navAppVer);
+                majorVersion = parseInt(navAppVer, 10);
+            }
+
+            return {
+                name: browser,
+                version: majorVersion,
+                versionString: version,
+                productVendor: vendor
+            };
+        }
+
+        // Получение информации о операционной системе
+        //надо еще покопаться
+        function getOS() {
+
+            var osVersion = unknown;
+            var os = unknown;
+
+            var clientStrings = [
+                { s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/ },
+                { s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/ },
+                { s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/ },
+                { s: 'Windows Vista', r: /Windows NT 6.0/ },
+                { s: 'Windows Server 2003', r: /(Windows NT 5.2|Windows Server 2003)/ },
+                { s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/ },
+                { s: 'Windows 2000 (SP1)', r: /Windows NT 5.01/ },
+                { s: 'Windows 2000', r: /(Windows NT 5.0|Windows 2000)/ },
+                { s: 'Windows ME', r: /(Win 9x 4.90|Windows ME)/ },
+                { s: 'Windows 98', r: /(Windows 98|Win98)/ },
+                { s: 'Windows 95', r: /(Windows 95|Win95|Windows_95)/ },
+                { s: 'Windows 3.11', r: /Win16/ },
+                { s: 'Windows CE', r: /Windows CE/ },
+                { s: 'Windows NT 4.0', r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/ },
+                { s: 'Windows ME', r: /Windows ME/ },
+                { s: 'Android', r: /Android/ },
+                { s: 'Open BSD', r: /OpenBSD/ },
+                { s: 'Sun OS', r: /SunOS/ },
+                { s: 'Linux', r: /(Linux|X11)/ },
+                { s: 'iOS', r: /(iPhone|iPad|iPod)/ },
+                { s: 'Mac OS X', r: /Mac OS X/ },
+                { s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/ },
+                { s: 'QNX', r: /QNX/ },
+                { s: 'UNIX', r: /UNIX/ },
+                { s: 'BeOS', r: /BeOS/ },
+                { s: 'OS/2', r: /OS\/2/ },
+                { s: 'Search Bot', r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/ }
+            ];
+            for (var id in clientStrings) {
+                var cs = clientStrings[id];
+                if (cs.r.test(navUsrAgt)) {
+                    os = cs.s;
+                    break;
+                }
+            }
+
+            if (/Windows/.test(os)) {
+                osVersion = /Windows (.*)/.exec(os)[1];
+                os = 'Windows';
+            }
+
+            switch (os) {
+                case 'Mac OS X':
+                    osVersion = /Mac OS X (10[\.\_\d]+)/.exec(navUsrAgt)[1];
+                    break;
+
+                case 'Android':
+                    osVersion = /Android ([\.\_\d]+)/.exec(navUsrAgt)[1];
+                    break;
+
+                case 'iOS':
+                    osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(navAppVer);
+                    osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+                    break;
+
+            }
+
+            return {
+                name: os,
+                versionString: osVersion
+            };
+        }
+
+        //stackTrace еще не готов
+        function stackTrace() {
+            try {
+                var err = new Error();
+                throw err;
+            }catch(e){
+                return e.stack;
+            }
+
+        }
+
+        //apiKey еще не готов
+        if (!apiKey){
+            throw new Error("need to call init method and give ApiKey to him ||" +
+            " Требуется вызвать метод init и передать ему ключ API")
+
+        }
+
+
         var data = JSON.stringify({
-            message: "test message",
-            url: window.location.toString(),
-            agent: navigator.userAgent,
-            browser: navigator.appName,
-            apiKey:apiKey
+        //Сводка
+            message: "Page not found",
+            date: getTime().Date,
+
+        //Подробно
+            stack: stackTrace(),
+
+        //Среда
+            browser: getBrowserInfo().name + " " + getBrowserInfo().version,
+            browserVendor: getBrowserInfo().productVendor,
+            browserVersion: getBrowserInfo().versionString,
+            agent: navUsrAgt,
+            browserResolution: getScreenResolution().browserResolution,
+            browserLanguage: navigator.language,
+            browserCookie: navigator.cookieEnabled,
+            browserJava: navigator.javaEnabled(),
+            platform: getPlatformInfo().platformString,
+            platformVersion: getOS().name + " " + getOS().versionString ,
+            screenResolution: getScreenResolution().nativeScreenResolution,
+
+        //Служебная информация
+            client: "RTF-Error-Tracker 0.1",
+            apiKey: apiKey,
+
+        //Запрос
+            url: location.toString(),
+            host: document.domain
+
+
         });
         $.ajax ({
             url: "/api/log",
